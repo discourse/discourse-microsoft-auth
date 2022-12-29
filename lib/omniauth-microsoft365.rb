63ac643ca59240d511f37046c30b169edf768537
David Taylor <david@taylorhq.com>
2022-12-29 10:15:51 +0000

    DEV: Introduce syntax_tree for ruby formatting

--- lib/omniauth-microsoft365.rb
+++ lib/omniauth-microsoft365.rb
@@ -10,9 +10,9 @@ module OmniAuth
       DEFAULT_SCOPE = "openid email profile https://graph.microsoft.com/User.Read"
 
       option :client_options,
-        site: 'https://login.microsoftonline.com',
-        authorize_url: '/common/oauth2/v2.0/authorize',
-        token_url: '/common/oauth2/v2.0/token'
+             site: "https://login.microsoftonline.com",
+             authorize_url: "/common/oauth2/v2.0/authorize",
+             token_url: "/common/oauth2/v2.0/token"
 
       option :authorize_options, [:scope]
 
@@ -20,27 +20,21 @@ module OmniAuth
 
       info do
         {
-          name: raw_info['displayName'] || raw_info['userPrincipalName'],
-          email: raw_info['mail'] || raw_info['userPrincipalName']
+          name: raw_info["displayName"] || raw_info["userPrincipalName"],
+          email: raw_info["mail"] || raw_info["userPrincipalName"],
         }
       end
 
-      extra do
-        {
-          "raw_info" => raw_info
-        }
-      end
+      extra { { "raw_info" => raw_info } }
 
       def raw_info
-        @raw_info ||= access_token.get('https://graph.microsoft.com/v1.0/me').parsed
+        @raw_info ||= access_token.get("https://graph.microsoft.com/v1.0/me").parsed
       end
 
       def authorize_params
         super.tap do |params|
           %w[display score auth_type].each do |v|
-            if request.params[v]
-              params[v.to_sym] = request.params[v]
-            end
+            params[v.to_sym] = request.params[v] if request.params[v]
           end
 
           params[:scope] ||= DEFAULT_SCOPE
@@ -61,7 +55,6 @@ module OmniAuth
       #     :expires_at => oauth2_access_token.expires_at
       #   })
       # end
-
     end
   end
 end
